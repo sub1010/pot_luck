7b285e6233fa9921282b63121ec330804157885c
Subal Charla <subalcharla@gmail.com>
2012-08-08 16:26:22 +0530

    Add a fullTitle helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
-module ApplicationHelper
+module ApplicationHelper 
+  
+  #This represents a helper that would available throughout all views of the applcaicatiomn
+  
+  
+  #The following method prints the full title of the page depending on the content
+  
+  def fullTitle(pageTitle)
+    baseTitle = PotLuck
+    if pageTitle.empty?
+      baseTitle
+    else
+      "#{baseTitle} | #{pageTitle}"
+    end
+  end
 end
